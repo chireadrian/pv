b905ba7f0d3050dcc8385b7a2454b722d4c4b826
chireadrian <chireadrian@gmail.com>
2017-05-13 00:13:49 +0300

    modificari rec stelu

--- pv_add.php
+++ pv_add.php
@@ -1,49 +1,6 @@
-<!DOCTYPE HTML PUBLIC>
-<html>
-<!--  Created with the CoffeeCup HTML Editor 2008  -->
-<!--           http://www.coffeecup.com/           -->
-<!--        Brewed on 3/25/2017 12:18:15 PM        -->
-<head>
-  <title></title>
-  <meta http-equiv="content-type" content="text/html;charset=UTF-8" />
-  <link rel="stylesheet" href="pv_css.css">
-</head>
-<body>
 
-<div id="sus" class="stil_div_pv">
-</div>
-
-<div id="mijloc" class="stil_div_pv">
-
-<div style="width:100%;height:35px;background-color:#696565;border-bottom:1px solid #929191;">
-  <ul class="pv_meniu">
-    <li><a href="pv.php">HOME</a></li>
-    <li><a href="pv_pv.php">PROCESE-VERBALE</a></li>
-    <li><a href="pv_add.php" style="background-color:#292525;">ADAUGA PROCES VERBAL</a></li>
-    <li><a href="pv_stat.php">STATISTICI</a></li>  
-    <li><a href="pv_config.php">CONFIGURARE</a></li>
-    <li><a href="pv_cont.php">CONTACT</a></li>
-  </ul>
-</div>
-<br />
+<?php
+include ('add_pv/html/header_add_pv.html');
 
 
-<form>
-
-<a href="goog.com" disable="disabled">aaaa</a>
-
-</form>
-
-
-
-</div>
-
-
-<div id="jos" class="stil_div_pv">
-</div>
-
-
-
-</body>
-</html>
-
+include ('footer.html');
