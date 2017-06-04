fc0310a5f4c334ae789ebdb6f2e62647f9dcec5c
chireadrian <chireadrian@gmail.com>
2017-06-04 18:53:31 +0300

    duminica

--- add_pv/gen_pv.php
+++ add_pv/gen_pv.php
@@ -3,12 +3,14 @@
 $query="select * from pvuri where pv_nou='DA'";
 $caut=mysqli_query($con,$query);
 $rez=mysqli_fetch_array($caut,MYSQLI_ASSOC);
+
+if ($rez['pv_nou']=="DA"){
 echo "1-->".$rez['l1']."&nbsp;2-->".$rez['l2']."&nbsp;3-->".$rez['l3']."&nbsp;4-->".$rez['l4'];
 include ("html\gen_pv.html");
 
 
 
-$query4=mysqli_query($con,"select c_reg,c_scan from pvdate where ev_pv='$id'");
+$query4=mysqli_query($con,"select c_reg from pvdate where ev_pv='$id'");
 $a1=null;
 $a2=null;
 $a3=null;
@@ -34,9 +36,7 @@ if (!is_null($rez4['c_reg'])){
                            }
 
                               }
-if (!is_null($rez4['c_scan'])){
-   echo $rez4['c_scan']."<br />";
-                              }							  
+					  
 							  
 							  
 							  
@@ -52,92 +52,66 @@ echo "Altele:".$altele."<br />";
 if ($altele=="DA"){
    $query5=mysqli_query($con,"select * from pv_altele where id_altele='$id'");
    while ($rez5=mysqli_fetch_assoc($query5)){ 
-   echo $rez5['defectiuni']."<br />";
-                                            }
 
-}
-
-
-/*
-$query="select * from pvuri where pv_nou='DA'";
-$caut=mysqli_query($con,$query);$rezultat=mysqli_fetch_array($caut,MYSQLI_ASSOC);
-if ($rezultat['pv_nou']=="DA"){  $g1="";  $g2="";  $g3="";  $g4=""; $l1="";  $l2="";$l3="";$l4="";
-  $query1="select grad_s,lucratori from pvuri,agenti where(l1=id or l2=id) and (pv_nou='DA' and activ='DA')";
-  $caut1=mysqli_query($con,$query1); 
-  $cont=0;  while ($rezultat1=mysqli_fetch_array($caut1,MYSQLI_ASSOC)){
-   echo $rezultat1['grad_s']." - ";
-   echo $rezultat1['lucratori']." <br /> ";
-   $cont++;
-   echo "<h3>".$cont."</h3>";
-   if ($cont==1) {
-   $g1=$rezultat1['grad_s'];
-   $l1=$rezultat1['lucratori'];
-               }
-   if ($cont==2){
-   $g2=$rezultat1['grad_s'];
-   $l2=$rezultat1['lucratori'];
-               } 
+   if (!is_null($rez5['c_scan'])){
+      echo "-->".$rez5['c_scan']."<br />";
+                              }		 
+   if (!is_null($rez5['defectiuni'])){
+      echo $rez5['defectiuni']."<br />";
+                              }		
  
-  }  $query1="select grad_s,lucratori from pvuri,agenti where(l3=id or l4=id) and (pv_nou='DA' and activ='DA')";
-  $caut1=mysqli_query($con,$query1); 
-  $cont=0;  while ($rezultat1=mysqli_fetch_array($caut1,MYSQLI_ASSOC)){
-   echo $rezultat1['grad_s']." - ";
-   echo $rezultat1['lucratori']." <br /> ";
-   $cont++;
-   echo "<h3>".$cont."</h3>";
-   if ($cont==1) {
-   $g3=$rezultat1['grad_s'];
-   $l3=$rezultat1['lucratori'];
-               }
-   if ($cont==2){
-   $g4=$rezultat1['grad_s'];
-   $l4=$rezultat1['lucratori'];
-               } 
-}  
-  
-  $query3="select tip_tura,pv_data from pvuri where pv_nou='DA'";
-  $caut3=mysqli_query($con,$query3);   
-  while ($rezultat3=mysqli_fetch_array($caut3,MYSQLI_ASSOC)){ 
-   echo $rezultat3['tip_tura']." - ";
-   echo $rezultat3['pv_data']." <br /> ";
-   $datestamp=strtotime($rezultat3['pv_data']);
-   $data=date("d.m.Y",$datestamp);
-                                     } 
- echo "<h3>PROCES - VERBAL</h3>";
- echo "Incheiat azi&nbsp;".$data."&nbsp;intre noi,&nbsp;".$g1."&nbsp;<strong>".$l1."</strong>&nbsp;".$g2."&nbsp;<strong>".$l2;
- echo "</strong>&nbsp;si&nbsp;".$g3."&nbsp;<strong>".$l3."</strong>&nbsp;".$g4."&nbsp;<strong>".$l4."</strong>";echo "&nbsp;procedand primul/primii la predarea si secundul/secunzii la primirea tehnicii si materialelor de comunicatii din cadrul Compartimentului Comunicatii si Informatica.";
- echo "<br />Cu ocazia predarii-primirii au afost aduse la cunostinta si activitatile desfasurate dupa cum urmeaza:<br /><br />";
+                                             }
+}
 
-   
-}else{echo "Nu exista activ pv";}  
+}else{
 
-*/
+echo "<h3> GENERARE PROCES - VERBAL</h3><br /><br />"; 
+
+$timp=mysqli_query($con,"select now()");
+$rez_t=mysqli_fetch_array($timp,MYSQLI_NUM);
+
+$time = strtotime($rez_t[0]);
+$creat1= date("d.m.Y", $time);
+echo $creat1;
 
 
-/*
-$query="select * from agenti where activ='DA'";
-$query1=mysqli_query($con,$query);
-while($rez=mysqli_fetch_array($query1,MYSQLI_ASSOC)){echo "<input type='checkbox' />" ;
-echo $rez['lucratori'];
+
+echo "<div class='div_genpv2'>";
+  echo "Introduceti data si ora iesirii din serviciu!<br /><br />";
+    echo "<form><input type='date' style='width:80px;' value='".$creat1."' />";
+    echo " <input type='radio' name='tura' value='NU' /><span>07:00</span>";
+    echo "<input type='radio' name='tura' value='DA' /><span>20:00</span> </form>";
  
- 
-echo "<br />";}
-$url= str_replace("?","&",$_SERVER['REQUEST_URI']);$id=explode('&' , $url );for ($x=0; $x<($cont+1)*2;$x++){if (isset($id[$x])){ 
-$z=substr($id[$x],0,2);
-if ($z=="a="){
-   $d=substr($id[$x],2);
-   $querya=mysqli_query($con,"select lucratori from agenti where id='$d'");
-   $nume=mysqli_fetch_array($querya,MYSQLI_ASSOC);
-   echo $nume['lucratori'];   echo $id[$x]."<br>";
-   echo substr($id[$x],0,2)."<br>";
+echo "</div>";
+
+
+echo "<div class='div_genpv1'>";
+  echo "Selectati agentii (max.2) care predau serviciul!  <br /><br />";
+
+$query7="select * from agenti where activ='DA'";
+$caut7=mysqli_query($con,$query7);
+
+echo "<form action='' method='GET'>";
+while($rez7=mysqli_fetch_array($caut7,MYSQLI_ASSOC)){
+
+echo "<label class='lab'><input type='checkbox' name='check_list[]' value='".$rez7['lucratori']."' /><span>".$rez7['lucratori']."</span></label><br />";
+
 }
-if ($z=="b="){
-   $d=substr($id[$x],2);
-   $querya=mysqli_query($con,"select lucratori from agenti where id='$d'");
-   $nume=mysqli_fetch_array($querya,MYSQLI_ASSOC);
-   echo $nume['lucratori'];   echo $id[$x]."<br>";
-   echo substr($id[$x],0,2)."<br>";
+
+foreach($_GET['check_list'] as $check) {
+echo "-".$check."<br />";
+
 }
-	}}
-}
-*/
+echo count($_GET['check_list'])."<br />";
+echo $_GET['check_list'][1];
+
+echo "<BR /><br /></div>";
+
+echo "<div class='div_genpv3'>";
+echo "<input type='submit' value='Genereaza PV!'> ";
+echo "</form>";
+
+echo "</div>";
+
+
+    }
